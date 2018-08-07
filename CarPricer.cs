f1b790ad447d999fc42da25536097d0ef1fa14d0
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-08-07 09:34:02 -0400

    change

--- CarPricer.cs
+++ CarPricer.cs
@@ -24,49 +24,7 @@ namespace CarPricer
         {
             //decimal carValue =0;
 
-            if (car.AgeInMonths < 11 * 12)
-            {
-                decimal localValue = ((car.PurchaseValue * 0.5m) / 100) * car.AgeInMonths;
-                //decimal carValue = ((car.PurchaseValue * 0.5m) / 100);
-
-                car.PurchaseValue = car.PurchaseValue - localValue;
-            }
-
-            
-                int numberOfMiles = car.NumberOfMiles;
-
-                if (car.NumberOfMiles > 150000)
-                    car.NumberOfMiles = 150000;
-
-                int milesPerThousand = car.NumberOfMiles / 1000;
-
-                decimal localValue1 = ((car.PurchaseValue * 0.2m) / 100) * milesPerThousand;
-
-                car.PurchaseValue = car.PurchaseValue - localValue1;
-
-            
-
-            if (car.NumberOfPreviousOwners > 2)
-            {
-                decimal localValue = ((car.PurchaseValue * 25) / 100);
-                car.PurchaseValue = car.PurchaseValue - localValue;
-            }
-            if ( car.NumberOfPreviousOwners == 0)
-            {
-                decimal localValue = ((car.PurchaseValue * 10) / 100);
-                car.PurchaseValue = car.PurchaseValue + localValue;
-
-            }
-
-            if (car.NumberOfCollisions < 6)
-            {
-                decimal localValue = ((car.PurchaseValue * 2) / 100) * car.NumberOfCollisions;
-
-                car.PurchaseValue = car.PurchaseValue - localValue;
-            }
-
-            return car.PurchaseValue;
-        }
+        
 
     }
     
